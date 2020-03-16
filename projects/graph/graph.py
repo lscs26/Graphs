8a61e88631e5b7e139cc63f1cc0a160ab07243de
kenna <kennaalastair@gmail.com>
2020-03-16 16:39:43 -0700

    btf complete

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,26 +13,46 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertex does not exist")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        queue = Queue()
+        # Put the starting point there
+        queue.enqueue(starting_vertex)
+        # Make a set to keep track of where we've been
+        visited = set()
+        # While there is stuff in the queue/stack
+        while queue.size() > 0:
+            # Pop the first item
+            vertex = queue.dequeue()
+            # If not visited
+            if vertex not in visited:
+                # Do the thing
+                print(vertex)
+                visited.add(vertex)
+                # For each edge in the item
+                for next_vert in self.get_neighbors(vertex):
+                    # Add that edge to the queue/stack
+                    queue.enqueue(next_vert)
 
     def dft(self, starting_vertex):
         """
