9607b7acfca045143fcf5d282c776da50b4ad5a9
kenna <kennaalastair@gmail.com>
2020-03-17 16:59:29 -0700

    graph created for ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,15 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # Build the graph
+    graph = Graph()
+
+    for parent, child in ancestors:
+    if parent not in graph.vertices:
+        graph.add_vertex(parent)
+    if child not in graph.vertices:
+        graph.add_vertex(child)
+    # Build in reverse order
+    graph.add_edge(child, parent)
+
+    # Do a BFS
+    
